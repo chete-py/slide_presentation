13ce815abe0dca187c3eb181e4c5deb5513fbb36
Collins Chetekei <112887160+chete-py@users.noreply.github.com>
2024-05-03 14:25:29 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,13 +33,25 @@ sample_markdown = r"""
 
 ---
 
-#### Market Players in the Insurance Ecosystem
+#### Market Players in The Insurance Ecosystem
 - Insurance Companies - APA , Old Mutual , Heritage , Fidelity , Jubilee <!-- .element: class="fragment" data-fragment-index="0" -->
 - Re-Insurance Companies - Africa Re , WAICA Re , Kenya Re , ZEP Re <!-- .element: class="fragment" data-fragment-index="1" -->
-- Brokers - Gras Savoye-WTW , AON-Minet , Zamara , Kenbright , Acentria <!-- .element: class="fragment" data-fragment-index="2" -->
+- Brokers  <!-- .element: class="fragment" data-fragment-index="2" -->
 - Consultancy - BIG 4 , ActServe , Zamara ,  Kenbright, Lux <!-- .element: class="fragment" data-fragment-index="3" -->
 - Regulator - IRA <!-- .element: class="fragment" data-fragment-index="4" -->
 ---
+
+### Roles of Intermediaries In The Insurance Ecosystem
+- What are Intermediaries and types of intermediaries? <!-- .element: class="fragment" data-fragment-index="0" -->
+- Examples of Intermediaries in the Insurance Landscape in Kenya? <!-- .element: class="fragment" data-fragment-index="1" -->
+    . GRAS SAVOYE - WTW <!-- .element: class="fragment" data-fragment-index="0" -->
+    . AON MINET <!-- .element: class="fragment" data-fragment-index="1" -->
+    . ACENTRIA <!-- .element: class="fragment" data-fragment-index="2" -->
+    . ZAMARA <!-- .element: class="fragment" data-fragment-index="3" -->
+- How do we earn our money? <!-- .element: class="fragment" data-fragment-index="2" -->
+- Key attributes to excel in this space? <!-- .element: class="fragment" data-fragment-index="3" -->
+- How descent is the salary? <!-- .element: class="fragment" data-fragment-index="4" -->
+
 
 #### Tips on How to Breakthrough
 - Luck  😂 😂 <!-- .element: class="fragment" data-fragment-index="0" -->
